2ea22917bcbd5f00fdceffc7b5d4938cfbcc8570
Heverton Coneglian de Freitas <hevertonconeglian@gmail.com>
2022-09-21 19:54:08 -0300

    Initial Commit

---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by Heverton Coneglian de Freitas on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
